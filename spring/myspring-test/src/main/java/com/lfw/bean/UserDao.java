3c68b803ed139a860052ecff1cdcca3eb5c12b8b
lfw <differentlee1012@163.com>
2024-10-16 18:58:11 +0800

    add 20241016 spring

---
+++ spring/myspring-test/src/main/java/com/lfw/bean/UserDao.java
@@ -0,0 +1,7 @@
+package com.lfw.bean;
+
+public class UserDao {
+    public void insert() {
+        System.out.println("UserDao正在插入数据");
+    }
+}
---
+++ spring/myspring-test/src/test/java/MySpringTest.java
@@ -0,0 +1,14 @@
+import com.lfw.bean.UserService;
+import org.junit.Test;
+import org.myspringframework.core.ApplicationContext;
+import org.myspringframework.core.ClassPathXmlApplicationContext;
+
+
+public class MySpringTest {
+    @Test
+    public void testMySpring() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("myspring.xml");
+        UserService userServiceBean = (UserService) applicationContext.getBean("userServiceBean");
+        userServiceBean.save();
+    }
+}
