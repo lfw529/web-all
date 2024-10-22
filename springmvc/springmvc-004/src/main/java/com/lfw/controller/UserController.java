ac21dedf24b693f920c754cbbf6f2ad11e7df307
lfw <differentlee1012@163.com>
2024-10-22 19:02:26 +0800

    add 20241022

--- springmvc/springmvc-004/src/main/java/com/lfw/controller/UserController.java
+++ springmvc/springmvc-004/src/main/java/com/lfw/controller/UserController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 
@@ -14,7 +15,7 @@ public class UserController {
         return "register";
     }
 
-    @PostMapping(value = "/register")
+    /*@PostMapping(value = "/register")
     public String register(HttpServletRequest request) {
         // 通过当前请求对象获取提交的数据
         String username = request.getParameter("username");
@@ -22,6 +23,26 @@ public class UserController {
         String sex = request.getParameter("sex");
         String[] hobby = request.getParameterValues("hobby");
         System.out.println(username + "," + password + "," + sex + "," + Arrays.asList(hobby));
+        return "success";
+    }*/
+
+    @PostMapping(value = "/register")
+    public String register(
+            @RequestParam(value = "uname")
+            String a,
+            @RequestParam(value = "password")
+            String b,
+            @RequestParam(value = "sex")
+            String c,
+            @RequestParam(value = "hobby")
+            String[] d,
+            @RequestParam(name = "intro")
+            Integer e) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(Arrays.toString(d));
+        System.out.println(e);
         return "success";
     }
 }
