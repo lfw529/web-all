e28450aaf7164c1dca4db48479d7f956a039657e
lfw <differentlee1012@163.com>
2024-11-13 18:55:43 +0800

    add 202411013

---
+++ springboot/boot3-04-web/src/main/java/com/lfw/web/Boot304WebApplication.java
@@ -0,0 +1,12 @@
+package com.lfw.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot304WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot304WebApplication.class, args);
+    }
+}
---
+++ springboot/boot3-04-web/src/main/java/com/lfw/web/config/MyConfig.java
@@ -0,0 +1,46 @@
+package com.lfw.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+//@EnableWebMvc //禁用boot的默认配置
+@Configuration //这是一个配置类,给容器中放一个 WebMvcConfigurer 组件，就能自定义底层
+public class MyConfig implements WebMvcConfigurer {
+
+
+//    @Bean
+//    public WebMvcConfigurer webMvcConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override //配置静态资源
+//            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//                registry.addResourceHandler("/static/**")
+//                        .addResourceLocations("classpath:/a/", "classpath:/b/")
+//                        .setCacheControl(CacheControl.maxAge(1180, TimeUnit.SECONDS));
+//            }
+//
+//            @Override //配置拦截器
+//            public void addInterceptors(InterceptorRegistry registry) {
+//
+//            }
+//
+//            @Override //配置一个能把对象转为yaml的messageConverter
+//            public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//                converters.add(new MyYamlHttpMessageConverter());
+//            }
+//        };
+//    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        //保留以前规则
+//        //自己写新的规则。
+////        registry.addResourceHandler("/static/**")
+////                .addResourceLocations("classpath:/a/", "classpath:/b/")
+////                .setCacheControl(CacheControl.maxAge(1180, TimeUnit.SECONDS));
+//    }
+}
