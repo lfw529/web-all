c1edd55ca5044bbe3bd34810b74f0a09faef428a
lfw <differentlee1012@163.com>
2024-12-20 16:50:41 +0800

    add 20241220

--- minio/minio-demo/src/test/java/com/lfw/MinioApplicationTests.java
+++ minio/minio-demo/src/test/java/com/lfw/MinioApplicationTests.java
@@ -21,8 +21,135 @@ class MinioApplicationTests {
     @Resource
     private MinIOService minIOService;
 
+    @Resource
+    private MinioClient minioClient;
+
     @Test
     void contextLoads() {
         minIOService.testMinIOClient();
+    }
+
+    // 判断目录是否存在
+    @Test
+    void test01() throws Exception {
+        boolean isBucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket("myfile").build());
+        System.out.println("myfile目录是否存在：" + isBucketExists);
+    }
+
+    // 创建 bucket
+    @Test
+    void test02() throws Exception {
+        String bucketName = "myfile2";
+        boolean isBucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if (!isBucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        } else {
+            System.out.println("bucket已经存在，不需要创建.");
+        }
+
+        String policyJsonString = "{\"Version\" : \"2012-10-17\",\"Statement\":[{\"Sid\":\"PublicRead\",\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"*\"},\"Action\":[\"s3:GetObject\"],\"Resource\":[\"arn:aws:s3:::" + bucketName + "/*\"]}]}";
+        //创建存储桶的时候，设置该存储桶里面的文件的访问策略，运行公开的读；
+        minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
+                .bucket(bucketName)
+                .config(policyJsonString)//json串，里面是访问策略
+                .build());
+    }
+
+    // 查看 bucket 列表
+    @Test
+    void test03() throws Exception {
+        List<Bucket> bucketList = minioClient.listBuckets();
+        bucketList.forEach(bucket -> {
+            System.out.println(bucket.name() + " -- " + bucket.creationDate());
+        });
+    }
+
+    // 删除 bucket
+    @Test
+    void test04() throws Exception {
+        minioClient.removeBucket(RemoveBucketArgs.builder().bucket("myfile2").build());
+    }
+
+    //---------------------------------------------------------------------------
+
+    // 上传文件
+    @Test
+    void test05() throws Exception {
+        File file = new File("D:\\MinIO\\MinioClient.jpg");
+        ObjectWriteResponse objectWriteResponse = minioClient.putObject(PutObjectArgs.builder()
+                .bucket("myfile")
+                .object("test.jpg")
+                .stream(new FileInputStream(file), file.length(), -1)
+                .build()
+        );
+        System.out.println(objectWriteResponse);
+
+        ObjectWriteResponse objectWriteResponse2 = minioClient.uploadObject(UploadObjectArgs.builder()
+                .bucket("myfile")
+                .object("test2.jpg")
+                .filename("D:\\MinIO\\MinioClient.jpg")
+                .build()
+        );
+        System.out.println(objectWriteResponse2);
+    }
+
+    // 检查指定文件的状态
+    @Test
+    void test06() throws Exception {
+        StatObjectResponse statObjectResponse = minioClient.statObject(StatObjectArgs.builder()
+                .bucket("myfile")
+                .object("test.jpg")
+                .build()
+        );
+        System.out.println(statObjectResponse);
+    }
+
+    // 用于生成一个对象（文件）的签名URL，以便可以通过HTTP访问下载；
+    @Test
+    void test07() throws Exception {
+        String presignedObjectUrl = minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder()
+                .bucket("myfile")
+                .object("test.jpg")
+                .expiry(3, TimeUnit.MINUTES)  // 设置过期时间
+                .method(Method.GET)
+                .build());
+        System.out.println(presignedObjectUrl);
+    }
+
+    // 用于从指定的存储桶中下载文件；
+    @Test
+    void test08() throws Exception {
+        GetObjectResponse getObjectResponse = minioClient.getObject(GetObjectArgs.builder()
+                .bucket("myfile")
+                .object("test.jpg")
+                .build());
+
+        System.out.println(getObjectResponse.transferTo(new FileOutputStream("D:\\MinIO\\123.jpg")));
+    }
+
+    // 用于列出指定存储桶中的所有对象（文件）；
+    @Test
+    void test09() throws Exception {
+        Iterable<Result<Item>> listObjects = minioClient.listObjects(ListObjectsArgs.builder()
+                .bucket("myfile")
+                .build());
+
+        listObjects.forEach(itemResult -> {
+            try {
+                Item item = itemResult.get();
+                System.out.println(item.objectName());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    // 用于删除指定存储桶中的对象，需要指定存储桶名称和对象键；
+    @Test
+    void test10() throws Exception {
+        minioClient.removeObject(RemoveObjectArgs.builder()
+                .bucket("myfile")
+                .object("test.jpg")
+                .build());
     }
 }
